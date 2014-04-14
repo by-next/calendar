f4c2482ea3e6a1ed670aef7adf291f842380e35e
by-next <t_watanabe@AFN-t-watanabe.local>
2014-04-14 18:15:07 +0900

    calendar

--- calendar.php
+++ calendar.php
@@ -1,57 +1,51 @@
 <?php
-
 class Calendar{
-    protected $weeks = array();
-    protected $timeStamp;
-
-    $this->$timeStamp = strtotime($ym . '-01');
-
-    //もし不正な値の場合当月を表示
-    if ($this->$timeStamp === false) {
-        $this->$timeStamp = time();
-    }
+    public $weeks = array();
+    public $timeStamp;
 
     public function _construct($ym){
+        $this->timeStamp = strtotime($ym . '-01');
+
+        //もし不正な値の場合当月を表示
+        if ($this->timeStamp === false) {
+            $this->timeStamp = time();
+        }
+    }
+    public function create(){
+        //最終日
+        $lastDay = date('t',$this->timeStamp);
+
+        $weekday = date('w',mktime(0,0,0,date('m',$this->timeStamp),1,date('Y',$this->timeStamp)));
+
+        $week    = "";
+
+        $week   .= str_repeat("<td></td>", $weekday);
+
+        for ($day=1; $day <= $lastDay; $day++, $weekday++) { 
+            $week .= sprintf('<td class="youbi_%d">%d</td>', $weekday % 7, $day);
+
+            if ($weekday % 7 == 6 or $day == $lastDay) {
+                if (condition) {
+                    $week .= str_repeat('<td></td>', 6 - ($weekday % 7));
+                }
+                $this->weeks[] = '<tr>' . $week . '</tr>';
+                $week = '';
+            }
+        }
 
     }
-    public function getweeks(){
-
+    public function getWeeks(){
+        return $this->weeks;
     }
     public function prev(){
-
+        return date('Y-m',mktime(0,0,0,date('m',$this->timeStamp)-1,1,date('Y',$this->timeStamp)));//先月リンク
     }
     public function next(){
-
+        return date('Y-m',mktime(0,0,0,date('m',$this->timeStamp)+1,1,date('Y',$this->timeStamp)));//次月リンク
     }
     public function yearMonth(){
+        return date('Y',$this->timeStamp);//当月
 
     }
 }
-
-    //先月リンク
-    $prev = date('Y-m',strtotime('-1,month'));
-    //次月リンク
-    $next = date('Y-m',strtotime('+1,month'));
-
-    //最終日
-    $lastDay = date('t',$timeStamp);
-
-    $weekday = date('w',mktime(0,0,0,date('m',$timeStamp),1,date('Y',$timeStamp)));
-
-    $weeks = array();
-    $week  = "";
-
-    $week .= str_repeat("<td></td>", $weekday);
-
-    for ($day=1; $day <= $lastDay; $day++, $weekday++) { 
-        $week .= sprintf('<td class="youbi_%d">%d</td>', $weekday % 7, $day);
-
-        if ($weekday % 7 == 6 or $day == $lastDay) {
-            if (condition) {
-                $week .= str_repeat('<td></td>', 6 - ($weekday % 7));
-            }
-            $weeks[] = '<tr>' . $week . '</tr>';
-            $week = '';
-        }
-    }
 ?>

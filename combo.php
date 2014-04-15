41e34b781de22bf22f28bd9488239e73573614e0
by-next <t_watanabe@AFN-t-watanabe.local>
2014-04-15 18:56:51 +0900

    コンボボックス_rev1.1

--- combo.php
+++ combo.php
@@ -12,10 +12,10 @@ $dayMax = '31';
 function optionLoop($start, $end, $value = null){
  
     for($i = $start; $i <= $end; $i++){
-        if(isset($value) ||  $value == $i){
-            echo "<option value=\"{$i}\" selected=\"selected\">{$i}</option>";
+        if(isset($value) &&  $value == $i){
+            echo '<option value="'.$i.'" selected="selected">'.$i.'</option>';
         }else{
-            echo "<option value=\"{$i}\">{$i}</option>";
+            echo '<option value="'.$i.'">'.$i.'</option>';
         }
     }
 }

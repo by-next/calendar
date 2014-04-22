9c75852afb165485e261f7244f6fcb4c7258698d
by-next <t_watanabe@AFN-t-watanabe.local>
2014-04-22 15:59:13 +0900

    rev2.4

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
 //年月の指定があれば
-// if(isset($_GET['Y']) && $_GET['Y'] != '' && isset($_GET['m']) && $_GET['m'] != ''){
-//     $yyyy = $_GET['Y'];
-//     $mm =   $_GET['m'];
-// //指定がなければ本日の年月
-// }else{
-//     $yyyy = date('Y');
-//     $mm =   date('m');
-// }
+if (isset($_GET['year'])) {
+    $year = $_GET['year'];
+}
+if (isset($_GET['month'])) {
+    $month = $_GET['month'];
+}//指定がなければ当月
+else{
+    $year  = date('Y');
+    $month = date('m');
+}
 
-$year  = date('Y');//年取得
-$month = date('m');//月取得
-$day   = date('d');//日取得
+// $year  = date('Y');//年取得
+// $month = date('m');//月取得
+// $day   = date('d');//日取得
 
 $month_date       = date('t', mktime(0,0,0,$month,1,$year));//月の日数表示(4月なら30日分)
 $month_begin_cell = date('w', mktime(0, 0, 0, $month, 1, $year));//当月の曜日の数値取得
 $last_day         = date('w', mktime(0, 0, 0, $month, $month_date, $year));//月末の曜日の数値の取得
 $month_end_cell   = 6-$last_day;//空マス計算
 
-$calendars = array();
+$calendars = array();//カレンダー格納配列
 
-//var_dump($calendars);
-$calendar_count = 5;//カレンダー表示数
+$calendar_count = 3;//カレンダー表示数
 
 $half = floor($calendar_count/2);//真ん中にくる月計算
 
+$half_month = strtotime($year.$month.'01');//真ん中の月
 
+
+//カレンダー生成
 for($i=0; $i<$calendar_count; $i++){
 
     $count_num   = -$half + $i;//カレンダー表示数の半分の数値取得
@@ -41,19 +45,94 @@ for($i=0; $i<$calendar_count; $i++){
     );
 }
 
+//先月
 $prev = array(
-    'year' => date('Y', strtotime('prev month', $half)),
-    'month' => date('m', strtotime('', $half))
+    'year' => date('Y', strtotime('last month', $half_month)),
+    'month' => date('m', strtotime('last month', $half_month))
 );
+//次月
 $next = array(
-    'year' => date('Y', strtotime('next month', $half)),
-    'month' => date('m', strtotime('next month', $half))
+    'year' => date('Y', strtotime('next month', $half_month)),
+    'month' => date('m', strtotime('next month', $half_month))
 );
 
-//var_dump($calendars);
+//var_dump($half_month);
+// var_dump($month_date);
+// var_dump($last_day);
+// var_dump($calendar_count);
+
+//echo $nextmonth;
+
+//コンボボックス内表示年数
+// $yearMin = '1980';
+// $yearMax = '2040';
+// //1月〜12月
+// $monthMin = '1';
+// $monthMax = '12';
+//コンボボックス
+// function optionLoop($start, $end, $value = null){
+ 
+//     for($i = $start; $i <= $end; $i++){
+//         if(isset($value) &&  $value == $i){
+//             echo '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+//         }else{
+//             echo '<option value="'.$i.'">'.$i.'</option>';
+//         }
+//     }
+// }
 
 
 ?>
+
+ <?php
+// 祝日取得
+// function get_holidays_this_month($month){
+//     $holidays_url = sprintf(
+//             'http://www.google.com/calendar/feeds/%s/public/full-noattendees?start-min=%s&amp;start-max=%s&amp;max-results=%d&amp;alt=json' ,
+//             'japanese__ja@holiday.calendar.google.com' ,
+//             $year.$month.'-01' ,  // 取得開始日
+//             $year.$month.'-31' ,  // 取得終了日
+//             31            // 最大取得数
+//             );
+//     if ( $results = file_get_contents($holidays_url) ) {
+//             $results = json_decode($results, true);
+//             $holidays = array();
+//             foreach ($results['feed']['entry'] as $val ) {
+//                     $date  = $val['gd$when'][0]['startTime'];
+//                     $week = date('w',strtotime($date));
+//                     $title = $val['title']['$t'];
+//                     $holidays[$date] = $title;
+//                     if( $week == 0) {
+//                         $nextday = date('Y-m-d',strtotime('+1 day', strtotime($date)));
+//                         $holidays[$nextday] = '振替休日';
+//                     }
+//                     $before_yesterday = date('Y-m-d',strtotime('-2 day', strtotime($date)));
+//                     if(isset($holidays[$before_yesterday])){
+//                         $yesterday = date('Y-m-d',strtotime('-1 day', strtotime($date)));
+//                         $holidays[$yesterday] = '国民の休日';
+//                     }
+//             }
+//             ksort($holidays);
+//     }
+//     return $holidays;
+// }
+//var_dump($val);
+?>
+<?php
+//コンボボックスのループ設定
+function optionLoop($start, $end, $value = null){
+ 
+    for($i = $start; $i <= $end; $i++){
+        if(isset($value) &&  $value == $i){
+            echo '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+        }else{
+            echo '<option value="'.$i.'">'.$i.'</option>';
+        }
+    }
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -70,29 +149,15 @@ $next = array(
     </header><!-- /header -->
     <div class="cal_view">
         <div class="header_link">
-            <a href="<?php echo '?year='.$prev['year'].'$month='.$prev['month'] ?>" class="prev">先月</a>
-            <a href="">今月</a>
-            <a href="<?php echo '?year='.$next['year'].'$month='.$next['month'] ?>" class="next">次月</a>
-            <!-- <form method="$_GET"; >
-                <select name="yyyy">
-                <?php
-                    $min_year = 1990;
-                    $max_year = 2040;
-
-                    for($i = $min_year; $i <= $max_year; $i++){
-                        echo '<option value="'.$i.'"'; if($i == $yyyy) echo ' selected'; echo '>'.$i.'</option>'."\n";
-                    }
-                ?>
-                </select>年
-                <select name="mm">
-                <?php
-                    for($i = 1; $i <= 12; $i++){
-                        echo '<option value="'.$i.'"'; if($i == $mm) echo ' selected'; echo '>'.$i.'</option>'."\n";
-                    }
-                ?>
-                </select>月
-                <input type="submit" value="表示する">
-            </form> -->
+            <a href="<?php echo '?year='.$prev['year'].'&month='.$prev['month'] ?>" class="button medium">先月</a>
+            <a href="index.php" class="button medium">今月</a>
+            <a href="<?php echo '?year='.$next['year'].'&month='.$next['month'] ?>" class="button medium">次月</a>
+            <form method="get" action="<?php $_SERVER['PHP_SELF']; ?>">
+                <select name="year">
+                    <?php optionLoop('1995', '2030',date('Y'));?></select>年
+                    <select name="month"><?php optionLoop('1', '12', '6');?></select>月
+                <input type="submit" value="表示">
+            </form>
         </div>
         <div class="box">
             <?php foreach ($calendars as $calendar) :?>    

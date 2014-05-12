a9f91b8b0538a2b9d05684de64a39c1e5f89288c
by-next <t_watanabe@AFN-t-watanabe.local>
2014-05-12 10:51:45 +0900

    db

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+
+function db_connect(){
+// DB接続
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'cal';
+
+    // MySQLに接続し、データベースを選択
+    $connect = mysqli_connect($host, $user, $password, $database);
+
+    // 接続状況をチェック
+    if (mysqli_connect_errno()) {
+        die(mysqli_connect_error());
+    }
+    return $connect;
+}
+$db_connect = db_connect();
+
+?> 
